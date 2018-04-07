790d381280c7eb7eb185f741d446e5a1ebba32b8
Lucas Melin <lucas.melin@gmail.com>
2018-04-07 06:33:54 -0400

    Added layouts and method to convert currency.

--- app/src/main/java/com/lucasmelin/currencyconverter/ConvertCurrencyActivity.java
+++ app/src/main/java/com/lucasmelin/currencyconverter/ConvertCurrencyActivity.java
@@ -5,14 +5,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-public class DisplayMessageActivity extends AppCompatActivity {
+public class ConvertCurrencyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
+        setContentView(R.layout.activity_convert_currency);
 
-        // Get the Intent that started this activity and extract the string
+        // Get the Intent that started this activity and extract the conversion value
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
--- app/src/main/java/com/lucasmelin/currencyconverter/MainActivity.java
+++ app/src/main/java/com/lucasmelin/currencyconverter/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.lucasmelin.currencyconverter.MESSAGE";
@@ -15,10 +16,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Called when the user taps the Send button
+     * Called when the user selects a currency radio button
      */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
+    public void onRadioButtonClicked(View view) {
+        // Check if the button is now checked
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which currency was selected
+        switch (view.getId()) {
+            case R.id.radio_usd:
+                if (checked) {
+                    // Convert to USD
+                }
+                break;
+            case R.id.radio_eur:
+                if (checked) {
+                    // Convert to EUR
+                }
+                break;
+            case R.id.radio_cny:
+                if (checked) {
+                    // Convert to CNY
+                }
+                break;
+        }
+
+        Intent intent = new Intent(this, ConvertCurrencyActivity.class);
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
